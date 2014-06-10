461e358a3dd0360c2318b2b29dcfdec8e6e0eb7c
Simon Conseil <contact@saimon.org>
2014-06-10 16:03:55 +0200

    Simplify setup with entry_points.

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# 1. Redistributions of source code must retain the above copyright notice, this
-# list of conditions and the following disclaimer.
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
 #
 # 2. Redistributions in binary form must reproduce the above copyright notice,
 # this list of conditions and the following disclaimer in the documentation
@@ -15,36 +15,36 @@
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-from setuptools import setup, Command
-
-from distutils.command.build_py import build_py
+from setuptools import setup
 
 
 setup(name='psrecord',
       version='0.2.0.dev',
       description='Python package to record activity from processes',
-      long_description='',
+      long_description=open('README.md').read(),
       url='https://github.com/astrofrog/psrecord',
       license='Simplified BSD License',
       author='Thomas Robitaille',
       author_email='thomas.robitaille@gmail.com',
       packages=['psrecord', 'psrecord.tests'],
       provides=['psrecord'],
-      scripts=['scripts/psrecord'],
       requires=['psutil'],
-      cmdclass={'build_py': build_py},
+      entry_points={
+          'console_scripts': ['psrecord = psrecord:main']
+      },
       classifiers=[
-                   "Development Status :: 3 - Alpha",
-                   "Programming Language :: Python",
-                   "License :: OSI Approved :: BSD License",
-                  ],
-     )
+          "Development Status :: 3 - Alpha",
+          "Programming Language :: Python",
+          "License :: OSI Approved :: BSD License",
+      ],
+      )
